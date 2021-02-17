670ed45e525e34a33e49adf948ab89720dfcac60
Refentse-SovTech <79168205+Refentse-SovTech@users.noreply.github.com>
2021-02-17 10:59:33 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import logo from './sovtech_logo.png';
 import './App.css';
 
-
+//const client = new ApolloClient({
+//   uri: 'http://localhost:4000/graphql',
+// });
+// client
+//   .query({
+//     query: gql`
+//       {
+//         getCoins{
+//           id,
+//           symbol
+//           name
+//           nameid
+//           rank
+//           price_usd
+//           percent_change_24h
+//           percent_change_1h
+//           percent_change_7d
+//           price_btc
+//           market_cap_usd
+//           volume24
+//           volume24a
+//           csupply
+//           tsupply
+//           msupply
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result));
 
 function App() {
   return (

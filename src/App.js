36b39a5f88658ae5d6ddec0f8f56a277e4f2f2fc
Refentse-SovTech <79168205+Refentse-SovTech@users.noreply.github.com>
2021-02-17 10:01:10 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './sovtech_logo.png';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      Coinlore Crypto API
+
+        <img src={logo} className="App-logo" alt="logo" />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
